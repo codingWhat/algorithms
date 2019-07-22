76242e35a00700d7a1a5d0d33887353798002823
codingWhat <hw3978574@gmail.com>
2019-07-22 20:25:03 +0800

    fibonacci implement

--- recursive/factorial.php
+++ recursive/factorial.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mfw
- * Date: 4/21/19
- * Time: 4:30 PM
- */
+
+function factorial_recursive($num) {
+
+    if ($num < 0 ) throw new Exception("minus doesnt have factorial.");
+
+    if ($num == 0) return 1;
+
+    return $num * factorial_recursive($num - 1);
+}
+
+echo  factorial_recursive(100);
+
+echo PHP_EOL;
+echo  factorial_dynamic(99);
+
+function factorial_dynamic($num) {
+    if ($num < 0 ) throw new Exception("minus doesnt have factorial.");
+
+    $arr[1] = 1;
+
+    for ($i = 2; $i <= $num; $i++) {
+        $arr[$i] = $arr[$i - 1] * $i;
+    }
+
+    return $arr[$num];
+}
