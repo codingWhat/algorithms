900a5229986f1f66b7ad628705b5cd071023bddc
codingWhat <hw3978574@gmail.com>
2019-08-20 22:10:15 +0800

    test cycle queue

--- listx/QueueTest.php
+++ listx/QueueTest.php
@@ -2,33 +2,27 @@
 namespace listx;
 
 require dirname(__DIR__)  . '/autoload.php';
-$list = new Queue(4);
+
+//循环队列会浪费一个空间
+$list = new Queue(5);
 
 $list->enqueue(1);
 $list->enqueue(2);
 $list->enqueue(3);
 $list->enqueue(4);
-//var_dump($list->getItems());
-echo  $list->dequeue() . PHP_EOL;
-echo $list->dequeue() . PHP_EOL;
-$list->enqueue(5);
-$list->enqueue(6);
-//var_dump($list->getItems());
+$list->enqueue(5);  //无效
 
-echo $list->dequeue() . PHP_EOL;
+echo  $list->dequeue() . PHP_EOL;
 echo $list->dequeue() . PHP_EOL;
 
 $list->enqueue(6);
 $list->enqueue(7);
+echo $list->dequeue() . PHP_EOL;
+echo $list->dequeue() . PHP_EOL;
 
- $list->dequeue();
- $list->dequeue();
-$list->enqueue(8);
 $list->enqueue(9);
-$list->dequeue();
 $list->enqueue(10);
 $list->dequeue();
 $list->dequeue();
-$list->enqueue(20);
 $list->enqueue(30);
 var_dump($list->getItems());
--- stack/imitateBroswer.php
+++ stack/imitateBroswer.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mfw
- * Date: 4/21/19
- * Time: 4:28 PM
- */
