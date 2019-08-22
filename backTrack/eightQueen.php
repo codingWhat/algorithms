8ff99d921e2ff0d24818a8fe5dbbb76d0de9f0a9
codingWhat <hw3978574@gmail.com>
2019-08-22 20:29:44 +0800

    八皇后问题

--- backTrack/eightQueen.php
+++ backTrack/eightQueen.php
@@ -1,20 +1,68 @@
-          <?php
+<?php
 //利用回溯算法求解八皇后问题
+//任何两个皇后不能处于同一条横行，纵行或斜线上
 
 
+class Queen {
 
-function eightQueens($row, $col, $arr, $recorded){
+    public $result = [];
 
-        $item = $arr[$row];
-        for ($i = 0; $i < $col; $i++) {
+    public function cal8queen($row = 0)
+    {
+        if ($row == 8) {
+            $this->printItems();
+            return;
+        }
 
-            if (isOk($item[$i], $i, $recorded)) {
-
+        for ($col = 0; $col < 8; $col++) {
+            if ($this->isOk($row, $col)) {
+                $this->result[$row] = $col;
+                $this->cal8queen($row+1);
             }
         }
 
+    }
+
+    public function isOk($row, $col)
+    {
+
+        $left = $col - 1;
+        $right  = $col + 1;
+        for ($i = $row -1 ; $i >= 0; $i-- ) {
+
+                if ($this->result[$i] == $col)  return false;
+
+                if ($left >= 0 && $this->result[$i] == $left) return false;
+
+                if ($right < 8 && $this->result[$i] == $right) return false;
+
+                $left--;
+                $right++;
+
+        }
+
+
+        return true;
+    }
+
+    public function printItems()
+    {
+        for ($i = 0; $i < 8; $i++ ) {
+
+            for ($j = 0; $j < 8; $j++) {
+                if ($this->result[$i] == $j) {
+                    echo "Q";
+                }else {
+                    echo  "*";
+                }
+            }
+
+            echo PHP_EOL;
+        }
+
+        echo PHP_EOL . PHP_EOL;
+    }
 }
 
-function isOk() {
-
-}
+$q = new Queen;
+$q->cal8queen();
