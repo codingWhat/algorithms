6104d5e4df9e09b06c9b48218c1599eb5ca47dca
codingWhat <hw3978574@gmail.com>
2019-09-10 15:26:18 +0800

    commit dp

--- dynamicProgramming/longestCommonSubSequence.php
+++ dynamicProgramming/longestCommonSubSequence.php
@@ -16,7 +16,40 @@ function longestCommonSubSequence ($str1, $str2, $len1, $len2){
 }*/
 $str1 = "XMJYAUZ";
 $str2 = "MZJAWXU";
-echo lcsAdvanced($str1, $str2);
+echo lcs($str1, $str2);exit;
+function lcs($str1, $str2) {
+
+    $len1 = strlen($str1);
+    $len2 = strlen($str2);
+    $arr = [];
+
+    //$arr[$len1+1][$len2+1]
+    for ($i = 0; $i <= $len1; $i++) {
+        $arr[$i][0] = 0;
+    }
+    //最长的公共子序列，最长为较短的字符串，所以以较短字符串的长度为column长度，这里为了代码简短，忽略了这层校验
+    for ($j = 0; $j <= $len2; $j++) {
+        $arr[0][$j] = 0;
+    }
+
+
+    for ($i = 1; $i <= $len1; $i ++) {
+        for ($j = 1; $j <= $len2; $j++) {
+
+            if ($str1[$i-1] == $str2[$j-1]) {
+                $arr[$i][$j] =  $arr[$i-1][$j] + 1;
+            } else {
+                $arr[$i][$j] = max($arr[$i-1][$j], $arr[$i][$j-1]);
+            }
+        }
+    }
+    var_dump($arr);
+    return $arr[$len1][$len2];
+}
+
+$str1 = "XMJYAUZ";
+$str2 = "MZJAWXU";
+echo lcsSpaceOptimized($str1, $str2);
 function lcsAdvanced($str1, $str2) {
     $len1 = strlen($str1);
     $len2 = strlen($str2);
@@ -44,6 +77,6 @@ function lcsAdvanced($str1, $str2) {
             $prev[$k]  = $cur[$k];
         }
     }
-var_dump($cur);
+
     return $cur[$len2];
 }
